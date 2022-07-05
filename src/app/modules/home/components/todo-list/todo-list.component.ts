cb397a5e7710a77b7ec2c82aac7a63ab89a3cfc4
Raul Correia <raulvinicius245@gmail.com>
2022-07-05 10:39:22 -0300

    Verificação de lista vazia para visualização todo-list e delete button. Adicionado classe para bg img.

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+
+  public taskList: Array<TaskList> = [
+    { task: 'Minha task #1', checked: true }
+  ];
 
   constructor() { }
 
